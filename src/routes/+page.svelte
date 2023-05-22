6895340eb3f805cd5baec7ee54588a8c269d3e25
imskanand <the.anand3099@gmail.com>
2023-05-22 22:09:32 +0530

    feat/layout: Made a common layout for the client and invoices

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,1 @@
-<h1 class="text-3xl font-bold text-white">Welcome to the Svelte World.</h1>
-
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.black);
-	}
-</style>
+Home Page
