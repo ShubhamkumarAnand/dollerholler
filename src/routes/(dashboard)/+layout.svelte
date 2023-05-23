c8edfaa0b828e477910e0b18bad58e4fc2bc9b29
imskanand <the.anand3099@gmail.com>
2023-05-23 14:59:49 +0530

    LaLaLa....

--- src/routes/(dashboard)/+layout.svelte
+++ src/routes/(dashboard)/+layout.svelte
@@ -4,5 +4,7 @@
 
 <div class="grid min-h-screen grid-cols-12 bg-whisper md:gap-x-16">
 	<Navbar />
-	<main class="col-span-12 md:col-span-8 md:pt-10"><slot /></main>
+	<main class="col-span-12 md:col-span-8 md:pt-10">
+		<slot />
+	</main>
 </div>
