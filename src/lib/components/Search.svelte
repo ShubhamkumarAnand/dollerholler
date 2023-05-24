452e0586aa379ab21e16a36d37f56c6121dd44b4
imskanand <the.anand3099@gmail.com>
2023-05-24 12:43:32 +0530

    style/invoice_list: Adding style to the invoice list

---
+++ src/lib/components/Search.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Search from './Icon/Search.svelte';
+</script>
+
+<div class="relative flex w-full items-baseline gap-6">
+	<div class="mr-2 mt-auto text-pastelPurple">
+		<Search height={32} width={32} />
+	</div>
+	<input
+		type="text"
+		placeholder="Search by Keyword"
+		name="search"
+		class="w-72 border-b-2 border-dashed border-b-pastelPurple bg-transparent font-sansSerif text-xl text-black outline-none focus:border-solid focus:border-b-lavenderIndigo"
+	/>
+	<button class="font-sansSerif text-xl font-black text-pastelPurple">Search</button>
+</div>
